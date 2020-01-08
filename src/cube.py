f904d52cfc045563bee61638ad25f66a2b6d1f37
Simmarum <mateusz0831@gmail.com>
2020-01-08 21:28:46 +0100

    Add various colors to cube

--- src/cube.py
+++ src/cube.py
@@ -2,6 +2,21 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+colors = (
+    (1, 0, 0),
+    (0, 1, 0),
+    (0, 0, 1),
+    (1, 1, 0),
+    (1, 1, 1),
+    (0, 1, 1),
+    (1, 0, 1),
+    (0, 0, 0),
+    (0, 0, 1),
+    (1, 0, 0),
+    (1, 1, 1),
+    (0, 1, 1),
+)
+
 
 class Cube():
 
@@ -31,12 +46,29 @@ class Cube():
         (5, 7)
     )
 
+    surfaces = (
+        (0, 1, 2, 3),
+        (3, 2, 7, 6),
+        (6, 7, 5, 4),
+        (4, 5, 1, 0),
+        (1, 5, 7, 2),
+        (4, 0, 3, 6)
+    )
+
     def __init__(self):
         pass
 
-    def draw(self):
+    def draw(self, color):
+        glBegin(GL_QUADS)
+        for idx, surface in enumerate(self.surfaces):
+            for vertex in surface:
+                glColor3fv(colors[idx])
+                glVertex3fv(self.verticies[vertex])
+        glEnd()
+
         glBegin(GL_LINES)
         for edge in self.edges:
             for vertex in edge:
+                glColor3fv((0, 0, 0))
                 glVertex3fv(self.verticies[vertex])
         glEnd()
--- src/game_life.py
+++ src/game_life.py
@@ -22,4 +22,4 @@ class GameLife():
 
         glRotatef(60/self.fps, 180/self.fps, 60/self.fps, 60/self.fps)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        self.c.draw()
+        self.c.draw((0, 1, 0))
